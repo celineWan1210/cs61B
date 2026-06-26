3061347c6f4a5f04c668de7141828cedb6d29dfc
celineWan1210 <wanyingxuan1210@gmail.com>
2026-06-26 15:45:31 +0800

    Fix bug by checking using n

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -1,16 +1,17 @@
 /** Class that prints the Collatz sequence starting from a given number.
- *  @author YOUR NAME HERE
+ *  @author Wan Ying Xuan
  */
 public class Collatz {
 
     /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
-            return 1;
-        } else if (n == 5) {
-            return 3 * n + 1;
+        // start by checking if number is even
+        if (n % 2 == 0) {
+            // if even return n/2
+            return n / 2;
+            // else if odd return 3n+1
         } else {
-            return n * 2;
+            return 3 * n + 1;
         }
     }
 
