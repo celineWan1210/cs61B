f1cec364c4b61341106c89696a4bcde06e26b1e8
celineWan1210 <wanyingxuan1210@gmail.com>
2026-07-12 17:48:35 +0800

    Update by poinitng head to null instad of head.rest potinting to null

--- lab2/IntList/IntListExercises.java
+++ lab2/IntList/IntListExercises.java
@@ -10,11 +10,11 @@ public class IntListExercises {
      */
     public static void addConstant(IntList lst, int c) {
         IntList head = lst;
-        while (head.rest != null) {
+        // as long as head is not null, means not yet reach end of the list
+        while (head != null) {
             head.first += c;
             head = head.rest;
         }
-        head.first += c;
     }
 
     /**
