5d161fa7d19a55a2fd41141c6df25f530b0fbacd
celineWan1210 <wanyingxuan1210@gmail.com>
2026-07-18 18:48:25 +0800

    Test if remove last for corret and buggy list return the same thing

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,23 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> correctList = new AListNoResizing<>();
+        AListNoResizing<Integer> buggyList = new AListNoResizing<>();
+
+        // add 4,5,6 to correct list
+        correctList.addLast(4);
+        correctList.addLast(5);
+        correctList.addLast(6);
+        // add 4,5,6 to buggy list
+        buggyList.addLast(4);
+        buggyList.addLast(5);
+        buggyList.addLast(6);
+
+        // compare list
+        assertEquals(correctList.removeLast(), buggyList.removeLast());
+        assertEquals(correctList.removeLast(), buggyList.removeLast());
+        assertEquals(correctList.removeLast(), buggyList.removeLast());
+    }
 }
