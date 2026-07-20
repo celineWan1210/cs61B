5e52890f6b544be1e80b32a15582bb69b9bc58ae
celineWan1210 <wanyingxuan1210@gmail.com>
2026-07-20 13:49:25 +0800

    Functions to implememnt

--- proj1/deque/LinkedListDeque.java
+++ proj1/deque/LinkedListDeque.java
@@ -1,5 +1,37 @@
 package deque;
 
-public class LinkedListDeque {
+public class LinkedListDeque<T> {
+    // size of array
+    private int size;
+    // array of items
+    private T[] items;
 
+
+    /**
+     * A size of 100 0s are instantiate by default
+     */
+    public LinkedListDeque() {
+        items = (T[]) new Object[100];
+        size = 0;
+    }
+
+    public void addFirst(T item) {
+        //
+    }
+
+    public void addLast(T item) {
+        //
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+
+    }
 }
